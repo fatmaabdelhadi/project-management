42fe68e232ffb84dfb8da6e7a3ea186f5fc64d72
Ganna <mrsheaven777@gmail.com>
2024-03-16 22:30:32 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+ // fihgvp;wiedjd;oslsw
 function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
         </p>
         <a
           className="App-link"
