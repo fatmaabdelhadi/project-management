cca9b6770809ac040c6deeed6f4760d777e67a9c
Ganna <mrsheaven777@gmail.com>
2024-03-20 22:31:16 +0200

    - Added some npm dependencies - Added Assets folder for images - Upated App.js for routing stuff - Added some pages - Updated App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
- // fihgvp;wiedjd;oslsw
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>edfgjd[</h2>
+      <button>next</button>
+      <input type='text'></input>
+      <input type='text' className='active'></input>
     </div>
   );
 }
