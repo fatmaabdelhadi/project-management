f3871953cc49cf8600ce87999b215582bf80acb1
Fatma AbdelHadi <fatmaabdelhadi2002@gmail.com>
2024-07-03 01:24:02 +0300

    Created ContactUs Page

--- src/Components/Landing/ContactUs.jsx
+++ src/Components/Landing/ContactUs.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 import "./Contact.css";
-export default function ContactUs() {
-  document.title = "Contact Us"
 
-  return <div className="ContactPage"></div>;
+const developers = [
+  {
+    name: 'Fatma AbdelHadi',
+    linkedin: 'https://www.linkedin.com/in/fatmaabdelhad1',
+    github: 'https://github.com/fatmaabdelhadi'
+  },
+  {
+    name: 'Ganna Mohamed',
+    linkedin: 'https://www.linkedin.com/in/ganna-mohamed-b04807240/',
+    github: 'https://github.com/GannaMohamed'
+  },
+  {
+    name: 'Hadeer Mamdouh',
+    linkedin: 'https://www.linkedin.com/in/hadeer-mam-43a5a324a/',
+    github: 'https://github.com/HadeerMamdouh'
+  }
+];
+
+export default function ContactUs() {
+  document.title = "Contact Us";
+  return (
+    <div className="contact-us contactPage">
+      <div className="developer-list">
+        {developers.map((developer, index) => (
+          <div key={index} className="developer">
+            <h3>{developer.name}</h3>
+            <div className="icons">
+            <p>
+              <a href={developer.linkedin} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </p>
+            <p>
+              <a href={developer.github} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github"></i>
+              </a>
+            </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
